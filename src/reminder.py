8a0a69401cca0429a8b205fa93cc5e165bd4f7dd
Ryan Niklas <rniklas25@gmail.com>
2022-09-20 15:23:26 -0400

    Reminders

--- src/reminder.py
+++ src/reminder.py
@@ -9,4 +9,4 @@ class PrefixedReminder:
 class PoliteReminder(PrefixedReminder):
     def __init__(self, text):
         super().__init__('Please remember to ')
-        self.text = self.prefix + text
+        self.text = self.prefix + text`
