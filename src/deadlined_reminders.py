cb2dad021d786fdd6538ab2a993705b9019c7e49
Ryan Niklas <rniklas25@gmail.com>
2022-09-22 08:11:53 -0400

    Reminders

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -23,4 +23,8 @@ class DateReminder(DeadlinedReminder):
         self.text = text
         self.date = parse(date, dayfirst=True)
 
+    def __iter__(self):
+        return iter ([self.text, self.date.isoformat()])
 
+    def is_due(self):
+        return self.date < datetime.now()
